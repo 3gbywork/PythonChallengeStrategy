0a18fcf9f8a8cb349462b55c770c55e827db3f60
3gbywork <614258809@qq.com>
2018-11-26 21:28:51 +0800

    更新子模块使用说明

--- Code/PowerShell/Challenge013.ps1
+++ Code/PowerShell/Challenge013.ps1
@@ -1,8 +1,13 @@
-# 安装XmlRpc模块
+# 安装XmlRpc模块（暂时未发布）
 # Install-Module -Name XmlRpc
 
 # 将XmlRpc源码作为git子模块克隆到Modules目录下
+# 添加子模块
 # git submodule add https://github.com/3gbywork/XmlRpc .\Code\PowerShell\Modules\XmlRpc
+# git submodule init
+# 更新子模块
+# git submodule init
+# git submodule update
 # 加载XmlRpc模块
 Import-Module .\Code\PowerShell\Modules\XmlRpc\XmlRpc.psm1
 
