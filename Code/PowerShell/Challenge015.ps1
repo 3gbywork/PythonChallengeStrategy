fb4334446d5e7fa0adbbaa73fb8282dc081e3476
3gbywork <614258809@qq.com>
2019-02-13 16:43:25 +0800

    move folders

--- Code/PowerShell/Challenge015.ps1
+++
@@ -1,9 +0,0 @@
-$LeapYears=@()
-for ($year = 1016; $year -le 1996; $year+=20) {
-    # 是闰年并且该年1月1日是星期四
-    if ([datetime]::IsLeapYear($year) -and [datetime]::new($year,1,1).DayOfWeek -eq [System.DayOfWeek]::Thursday) {
-        $LeapYears+=$year
-    }
-}
-
-$LeapYears[-2]
