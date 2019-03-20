6fb3f66d869e91ec294b1ce71253577a5ad5124c
3gbywork <614258809@qq.com>
2019-03-20 16:54:46 +0800

    remove docs and update gitignore

--- docs/Code/Python/Challenge015.py
+++
@@ -1,8 +0,0 @@
-import calendar
-
-leapYears=[]
-for year in range(1016, 1996+1, 20):
-    # 是闰年并且该年1月1日是星期四
-    if calendar.isleap(year) and calendar.weekday(year,1,1)==3:
-        leapYears.append(year)
-print(leapYears[-2])
